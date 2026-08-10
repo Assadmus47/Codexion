89f718ecfb5851ef12d6c2315e2f253e26ad1b41
Assadmus47 <assadkac47@gmail.com>
2026-08-10 04:59:55 +0200

    fix: makeing some clean code by fixing some norm message

--- includes/codexion.h
+++ includes/codexion.h
@@ -1,64 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   codexion.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkacemi <mkacemi@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/09 04:55:58 by mkacemi           #+#    #+#             */
+/*   Updated: 2026/08/09 04:56:02 by mkacemi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #ifndef CODEXION_H
 # define CODEXION_H
 
+# include <pthread.h>
+# include <stdint.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <string.h>
-# include <stdint.h>
-# include <pthread.h>
-# include <unistd.h>
 # include <time.h>
-
-/* parsing_utils.c */
-int	is_valid_number(const char *str);
-int is_digit(char c);
-size_t	ft_str_to_size(const char *str, int *valid);
-int is_valid_scheduler(const char *str);
-int	parse_args(int argc, char **argv, size_t *values);
-/* time_utils.c */
-size_t	get_timestamp_ms(void);
-struct timespec	get_future_timespec(size_t ms);
+# include <unistd.h>
 
 typedef struct s_simulation	t_simulation;
-typedef struct s_dongle	t_dongle;
-typedef struct s_heap_node t_heap_node;
-typedef struct s_heap t_heap;
-typedef struct s_coder t_coder;
-/* logging.c */
-void    log_message(t_simulation *sim, int coder_id, char *message);
-/* coder.c */
-void	*coder_routine(void *arg);
-void	*monitor_routine(void *arg);
-int 	get_simulation_flag(t_coder *coder);
-/* dongle.c*/
-int		use_dongle(t_dongle *dongle, size_t cooldown);
-void	release_dongle(t_dongle *dongle);
-int		acquire_dongles(t_coder *coder);
-void	release_dongles(t_coder *coder);
-/* heap.c */
-int	heap_init(t_heap *heap, size_t capacity);
-void	heap_push(t_heap *heap, t_coder *coder, size_t priority);
-t_coder	*heap_pop(t_heap *heap);
-void	heap_destroy(t_heap *heap);
-size_t	get_priority(t_coder *coder);
-/* time_utils.c */
-void	usleep_ms(size_t ms);
-/* dongle_wait.c*/
-int	acquire_one_dongle(t_coder *coder, t_dongle *dongle);
-
-typedef struct s_heap_node
-{
-	t_coder	*coder;
-	size_t	deadline;
-}	t_heap_node;
-
-typedef struct s_heap
-{
-	t_heap_node	*nodes;
-	size_t		size;
-	size_t		capacity;
-}	t_heap;
+typedef struct s_dongle		t_dongle;
+typedef struct s_heap_node	t_heap_node;
+typedef struct s_heap		t_heap;
+typedef struct s_coder		t_coder;
 
 typedef enum e_state
 {
@@ -73,7 +40,56 @@ typedef enum e_scheduler
 	EDF
 }	t_scheduler;
 
-typedef	struct	s_dongle
+/* parsing_utils.c */
+int				is_valid_number(const char *str);
+int				is_digit(char c);
+size_t			ft_str_to_size(const char *str, int *valid);
+int				is_valid_scheduler(const char *str);
+int				parse_args(int argc, char **argv, size_t *values);
+
+/* time_utils.c */
+size_t			get_timestamp_ms(void);
+struct timespec	get_future_timespec(size_t ms);
+void			usleep_ms(size_t ms);
+
+/* logging.c */
+void			log_message(t_simulation *sim, int coder_id, char *message);
+
+/* coder.c */
+void			*coder_routine(void *arg);
+void			*monitor_routine(void *arg);
+int				get_simulation_flag(t_coder *coder);
+
+/* dongle.c */
+int				use_dongle(t_dongle *dongle, size_t cooldown);
+void			release_dongle(t_dongle *dongle);
+int				acquire_dongles(t_coder *coder);
+void			release_dongles(t_coder *coder);
+
+/* heap.c */
+int				heap_init(t_heap *heap, size_t capacity);
+void			heap_push(t_heap *heap, t_coder *coder, size_t priority);
+t_coder			*heap_pop(t_heap *heap);
+void			heap_destroy(t_heap *heap);
+size_t			get_priority(t_coder *coder);
+
+/* dongle_wait.c */
+int				acquire_one_dongle(t_coder *coder, t_dongle *dongle);
+
+typedef struct s_heap_node
+{
+	t_coder	*coder;
+	size_t	deadline;
+}	t_heap_node;
+
+typedef struct s_heap
+{
+	t_heap_node	*nodes;
+	size_t		size;
+	size_t		capacity;
+}	t_heap;
+
+typedef struct s_dongle
 {
 	int				id;
 	int				is_taken;
@@ -83,20 +99,20 @@ typedef	struct	s_dongle
 	pthread_cond_t	cond;
 }	t_dongle;
 
-typedef	struct	s_coder
+typedef struct s_coder
 {
-	int			id;
-	t_state		coder_status;
-	size_t		nb_compiles;
-	size_t		last_compile_start;
-	t_dongle	*left_dongle;
-	t_dongle	*right_dongle;
+	int				id;
+	t_state			coder_status;
+	size_t			nb_compiles;
+	size_t			last_compile_start;
+	t_dongle		*left_dongle;
+	t_dongle		*right_dongle;
 	t_simulation	*sim;
 	pthread_t		coder_thread;
 	pthread_mutex_t	compile_mutex;
 }	t_coder;
 
-typedef	struct	s_simulation
+typedef struct s_simulation
 {
 	size_t			number_of_coders;
 	size_t			time_to_burnout;
@@ -109,8 +125,8 @@ typedef	struct	s_simulation
 	t_coder			*coders;
 	t_dongle		*dongles;
 	pthread_mutex_t	log_mutex;
-	int 			flag;
-	pthread_mutex_t flag_mutex;
+	int				flag;
+	pthread_mutex_t	flag_mutex;
 }	t_simulation;
 
 #endif
