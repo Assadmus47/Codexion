25f9a9563b83ae9a948ac4fcc4707056fb25c9ef
Assadmus47 <assadkac47@gmail.com>
2026-08-12 02:20:40 +0200

    fix: unsigned integer underflow in get_timestamp_ms elapsed calc
    - tv.tv_usec - start_tv.tv_usec could be negative when the
      microseconds field wraps across a second boundary, but both
      operands were size_t (unsigned) -> massive underflow value
      (~18446744073710000+), causing every burnout timestamp to
      print a garbage number and every no

--- srcs/time_utils.c
+++ srcs/time_utils.c
@@ -6,7 +6,7 @@
 /*   By: mkacemi <mkacemi@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/09 04:56:41 by mkacemi           #+#    #+#             */
-/*   Updated: 2026/08/12 02:02:53 by mkacemi          ###   ########.fr       */
+/*   Updated: 2026/08/12 02:06:53 by mkacemi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,14 +24,13 @@ static void	set_start_time(void)
 size_t	get_timestamp_ms(void)
 {
 	struct timeval	tv;
-	size_t			elapsed_sec;
-	size_t			elapsed_usec;
+	long long		diff_usec;
 
 	pthread_once(&init_flag, set_start_time);
 	gettimeofday(&tv, NULL);
-	elapsed_sec = tv.tv_sec - start_tv.tv_sec;
-	elapsed_usec = tv.tv_usec - start_tv.tv_usec;
-	return ((size_t)(elapsed_sec * 1000) + (elapsed_usec / 1000));
+	diff_usec = (long long)(tv.tv_sec - start_tv.tv_sec) * 1000000
+		+ (long long)(tv.tv_usec - start_tv.tv_usec);
+	return ((size_t)(diff_usec / 1000));
 }
 
 void	usleep_ms(size_t ms)
