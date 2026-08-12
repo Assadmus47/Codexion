bdb7486ea627313f30f7745506ef3bb4fbf808cb
Assadmus47 <assadkac47@gmail.com>
2026-08-12 02:04:07 +0200

    fix: log timestamps relative to program start instead of Unix epoch
    - get_timestamp_ms: capture start time once via pthread_once on
      first call (thread-safe against multiple coders starting
      near-simultaneously), return time elapsed since that reference
      instead of raw epoch value
    - check_coder: raise sim->flag BEFORE logging burned

--- srcs/time_utils.c
+++ srcs/time_utils.c
@@ -6,19 +6,32 @@
 /*   By: mkacemi <mkacemi@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/09 04:56:41 by mkacemi           #+#    #+#             */
-/*   Updated: 2026/08/09 04:57:03 by mkacemi          ###   ########.fr       */
+/*   Updated: 2026/08/12 02:02:53 by mkacemi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "codexion.h"
 #include <sys/time.h>
 
+static pthread_once_t	init_flag = PTHREAD_ONCE_INIT;
+static struct timeval	start_tv;
+
+static void	set_start_time(void)
+{
+	gettimeofday(&start_tv, NULL);
+}
+
 size_t	get_timestamp_ms(void)
 {
 	struct timeval	tv;
+	size_t			elapsed_sec;
+	size_t			elapsed_usec;
 
+	pthread_once(&init_flag, set_start_time);
 	gettimeofday(&tv, NULL);
-	return ((size_t)(tv.tv_sec * 1000) + (tv.tv_usec / 1000));
+	elapsed_sec = tv.tv_sec - start_tv.tv_sec;
+	elapsed_usec = tv.tv_usec - start_tv.tv_usec;
+	return ((size_t)(elapsed_sec * 1000) + (elapsed_usec / 1000));
 }
 
 void	usleep_ms(size_t ms)
